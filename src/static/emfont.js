e14124ce8afa05b02643f0aa86df5d6cff23b89a
Elvis Mao <info@elvismao.com>
2025-03-22 10:23:31 +0800

    feat: finished emfont.js

--- src/static/emfont.js
+++ src/static/emfont.js
@@ -1,68 +1,115 @@
-const emfont = (function () {
-    // Function to get all elements with class starting with .emfont and load the custom font
-    const loadCustomFonts = (config = {
-        
-    }, callback) => {
+class Emfont {
+    constructor(
+        config = {
+            caseSensitive: false,
+            weightSensitive: false,
+            weight: 400,
+            format: "woff2", // woff2, woff, ttf, eot
+            autoApply: true,
+            cache: true,
+            applyAt: document.head,
+        }
+    ) {
+        this.config = config;
     }
-        const elements = document.querySelectorAll("[class*='emfont']");
-        let fonts = {};
-        let promises = [];
-        elements.forEach(element => {
-            // Get all font names from element class
-            const fontNames = element.className
-                .split(" ")
-                .filter(name => name.startsWith("emfont-"))
-                .map(name => name.replace("emfont-", ""));
-            const words = element.textContent.trim(); // Custom words from element text
 
-            fontNames.forEach(fontName => {
-                if (fontName && words)
-                    fonts[fontName] =
-                        (fonts[fontName] ? fonts[fontName] : "") + words;
+    // a object to store fonts already loaded
+    fonts = {};
+
+    init() {
+        return new Promise(resolve => {
+            const elements = document.querySelectorAll("[class*='emfont']");
+            let fonts = {};
+            let promises = [];
+
+            elements.forEach(element => {
+                // Get all font names from element class
+                const fontNames = element.className
+                    .split(" ")
+                    .filter(
+                        name =>
+                            name.startsWith("emfont-") || name.startsWith("✏️")
+                    )
+                    .map(name => name.replace(/^(emfont-|✏️)/, ""));
+                const words = element.textContent.trim(); // Custom words from element text
+
+                fontNames.forEach(fontName => {
+                    if (fontName && words) {
+                        // check if there are -500, -500, -900, etc. in class name, must start with -
+                        const weight = fontName.match(/^(-?\d+)/)?.[0];
+                        const realWeight = this.config.weightSensitive
+                            ? weight
+                                ? parseInt(weight)
+                                : this.config.weight
+                            : this.config.weight;
+                        const fontClassName =
+                            fontName.replace(weight, "") + realWeight;
+                        fonts[fontClassName] =
+                            (fonts[fontClassName] ? fonts[fontClassName] : "") +
+                            words;
+                    }
+                });
+            });
+
+            const cssElement = document.createElement("style");
+            if (this.config.autoApply)
+                this.config.applyAt.appendChild(cssElement);
+
+            // Load custom fonts
+            const fetchPromises = Object.entries(fonts).map(
+                ([fontName, text]) => {
+                    const words = Array.from(new Set(text.split("")))
+                        .sort()
+                        .join("");
+                    let postFontName = fontName;
+                    // check if fontName contains -min
+                    const min = fontName.match(/-min/)?.[0];
+                    if (min) postFontName = fontName.replace(min, "");
+                    const weight = fontName.match(/-[0-9]+/)?.[0];
+                    if (weight) postFontName = fontName.replace(weight, "");
+                    return fetch("https://font.emtech.cc/g/" + postFontName, {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            words,
+                            min,
+                            weight,
+                            format: this.config.format,
+                        }),
+                    })
+                        .then(response => response.json())
+                        .then(data => {
+                            if (data.status === "success") {
+                                if (data.message) console.warn(data.message);
+                                const fontCSSName = data.name;
+                                const font = new FontFace(
+                                    fontCSSName,
+                                    data.location
+                                        .map(url => `url(${url})`)
+                                        .join(", ")
+                                );
+                                if (this.config.autoApply)
+                                    cssElement.innerHTML += `.emfont-${fontName} { font-family: '${fontCSSName}'; } 
+                                    .✏️${fontName} { font-family: '${fontCSSName}'; }`;
+                                // Add to the document.fonts (FontFaceSet)
+                                return font.load().then(loadedFont => {
+                                    document.fonts.add(loadedFont);
+                                });
+                            } else {
+                                console.error(data.message);
+                                return Promise.resolve();
+                            }
+                        });
+                }
+            );
+            // Wait for all fonts to load
+            Promise.all(fetchPromises).then(() => {
+                resolve();
             });
         });
-        const cssElement = document.createElement("style");
-
-        // Load custom fonts
-        for (const fontName in fonts) {
-            var textList = fonts[fontName];
-            // Remove duplicate characters and sort by UTF encoding
-            textList = Array.from(new Set(textList.split("")))
-                .sort()
-                .join("");
-            let promise = fetch("https://font.emtech.cc/g/" + fontName, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ words: textList }),
-            })
-                .then(response => response.json())
-                .then(data => {
-                    const fontCSSName = data.font.zh;
-                    const font = new FontFace(fontCSSName, `url(${data.url})`);
-                    cssElement.innerHTML += `.emfont-${fontName} { font-family: '${fontCSSName}', sans-serif; }`;
-                    // Add to the document.fonts (FontFaceSet)
-                    return font.load().then(loadedFont => {
-                        document.fonts.add(loadedFont);
-                    });
-                });
-            promises.push(promise);
-        }
-        document.head.appendChild(cssElement);
-
-        // Wait for all fonts to load
-        Promise.all(promises).then(() => {
-            if (callback && typeof callback === "function") {
-                callback();
-            }
-        });
     }
+}
 
-    return {
-        init: (config, callback) => {
-            loadCustomFonts(config, callback);
-        },
-    };
-})();
-/* eslint-enable no-unused-vars */
+const emfont = new Emfont();
