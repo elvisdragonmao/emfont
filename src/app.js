27b8a986270d434212f5a697827fad4ec8bbbf4f
EachChen <iach526526@gmail.com>
2025-03-29 20:57:44 +0800

    fix: update app.listen to use object syntax for port

--- src/app.js
+++ src/app.js
@@ -154,7 +154,7 @@ app.ready().then(initCheck);
 const start = async () => {
     try {
         const run_port = process.env.PORT || 3000;
-        app.listen(run_port, () => {
+        app.listen({port:run_port}, () => {
             console.log(`Server running at http://localhost:${run_port}`);
           });
     } catch (err) {
