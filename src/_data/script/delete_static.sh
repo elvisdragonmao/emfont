bfd43ae5dda6e707c221c23448b971333169555a
EachChen <iach526526@gmail.com>
2025-04-24 19:51:15 +0800

    test: 註解可疑的腳本

--- src/_data/script/delete_static.sh
+++ src/_data/script/delete_static.sh
@@ -1,2 +1,2 @@
-#$emfont/
-find ./src/_data/_generated  -mindepth 1 -maxdepth 1 -type d -exec rm -rf {} +
+# #$emfont/
+# find ./src/_data/_generated  -mindepth 1 -maxdepth 1 -type d -exec rm -rf {} +
