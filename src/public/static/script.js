17b39b32ebb82f79945499476f3ee27a40025394
Elvis Mao <info@elvismao.com>
2025-05-09 15:40:22 +0800

    fix: min if no input oppisite

--- src/public/static/script.js
+++ src/public/static/script.js
@@ -89,7 +89,7 @@ const updateFontDisplay = (e, animationOff = false) => {
         const matchTags = tags.length === 0 || tags.every(tag => font.tags.includes(tag));
         return matchName && matchFamily && matchCategory && matchTags;
     });
-    const min = (searchText.value) ? "-min" : "";
+    const min = (searchText.value) ? "" : "-min";
     const previewText = searchText.value || "我個人認為義大利麵就應該拌42號混泥土，因為這個螺絲釘的長度很容易直接影響到挖掘機的扭矩。";
     let containerHTML = "";
     // if filtered length is same as fontList length
@@ -293,7 +293,7 @@ const loadFontInfo = async fontId => {
         <label for="coverage-ko">韓文 (30%)</label>
         <div class="coverage-bar" id="coverage-ko" style="--percent: 30%"></div>
     </div>`;
-    const min = (searchText.value) ? "-min" : "";
+    const min = (searchText.value) ?  "": "-min";
     const inputText = searchText.value || "我個人認為義大利麵就應該拌42號混泥土，因為這個螺絲釘的長度很容易直接影響到挖掘機的扭矩。";
     weightContainer.innerHTML = "";
     font.weight.map(weight => {
