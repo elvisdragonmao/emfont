d8cddd8673503dfe7d4709a794406154bf136a78
Elvis Mao <info@elvismao.com>
2025-07-26 23:46:26 +0800

    feat: CSS sending res.type

--- src/website/api.js
+++ src/website/api.js
@@ -73,7 +73,7 @@ const registerApi = async (app, state) => {
                 if (req.body.weight && allWeights.includes(req.body.weight)) {
                     allWeights = [req.body.weight];
                 }
-                return res.send(
+                return res.type("text/css").send(
                     allWeights
                         .map(
                             weight => `@font-face {
@@ -92,7 +92,7 @@ const registerApi = async (app, state) => {
                 const response = await genFont(req, res, state);
                 if (response.code == 200) {
                     const urls = response.location.map(font => `url('${font}') format('woff2')`).join(",\n");
-                    return res.send(`@font-face {
+                    return res.type("text/css").send(`@font-face {
   font-family: '${response.name}';
   src: ${urls};
   font-weight: ${req.params.weight || "normal"};
