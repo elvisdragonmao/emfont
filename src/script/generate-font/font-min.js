4b6bba2128ce076bf747eb95129ac83a70b4a2fb
Each Chen <info@iach.cc>
2025-08-08 18:12:47 +0800

    feat: avoid gen empty subfont file

--- src/script/generate-font/font-min.js
+++ src/script/generate-font/font-min.js
@@ -1,7 +1,7 @@
 import path from "path";
 import fs from "fs";
 import subsetFont from "subset-font";
-import {readFontBuffer} from "../read-font-file/readFontBuffer.js"
+import {readFontBuffer,check_in_charArray} from "../read-font-file/readFontBuffer.js"
 const __dirname = import.meta.dirname;
 async function generateFont(
     originalFontFamily,
@@ -28,7 +28,18 @@ async function generateFont(
         const destFolder = path.join(__dirname, put_folder);
         fs.mkdirSync(destFolder, { recursive: true });
 
-        // // 輸出路徑
+        // 檢查是否請求的所有字該字型檔都生不出來，避免生成空的檔案。
+        const continue_gen=await check_in_charArray(originalFontFamily,font_weight,words);
+        if(!continue_gen)
+        {
+            //字型檔不存在請求的任何一個字元
+            return {
+            status: "failed",
+            message: "user input char doesn't exist in original font",
+            location: "null"
+        };
+        }
+        //如果部分字有缺仍然繼續生成，但會顯示貓爪
 
         // // 寫入檔案
         // fs.writeFileSync(outputPath, outBuffer);
