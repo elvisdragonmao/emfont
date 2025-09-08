248ce80606572e9af3daa7376207ec66c06abeeb
EachChen <iach526526@gmail.com>
2025-09-09 07:55:03 +0800

    feat: output css code range file

--- src/script/css-codpoint.js
+++ src/script/css-codpoint.js
@@ -1,5 +1,20 @@
 import { db } from "../utils/database.js";
-async function make_css_code_point(font_id, weight) {
+import path from "path";
+import { promises as fs } from "fs";
+async function writeCssFile(font_id, weight, cssBlocks) {
+	const __dirname = import.meta.dirname;
+	const put_folder = `../_data/css/${font_id}`;
+	const destFolder = path.join(__dirname, put_folder);
+	await fs.mkdir(destFolder, { recursive: true });
+	const fileName = `${destFolder}/${weight}.css`;
+	try {
+		await fs.writeFile(fileName, cssBlocks, "utf8");
+		console.log(`✅ 已產生 CSS 檔案: ${fileName}`);
+	} catch (err) {
+		console.error("❌ 寫檔失敗:", err);
+	}
+}
+async function make_css_code_point(font_id, weight, state) {
 	const { rows } = await db.query(
 		`select pack,string_agg("char" , '') AS chars  from static_fonts where $1 = any (families) group by pack`,
 		[font_id]
@@ -38,20 +53,21 @@ async function make_css_code_point(font_id, weight) {
 		};
 	});
 	// 產生 CSS
-	const cssBlocks = packs.map(
-		(p) => `@font-face {
-        font-family: '${font_id}';
-        font-style: normal;
-        font-weight: ${weight};
-        font-display: swap;
-        src: url(/fonts/${font_id}-${p.pack}.woff2) format('woff2');
-        unicode-range: ${p.unicodeRanges};
-    }`
-	);
-	return {
-		code: 200,
-		font_id,
-        cssBlocks
-	};
+	// src: url.
+	const cssBlocks = packs.map((p) => {
+		const paddedPack = String(p.pack).padStart(3, "0");
+		return `@font-face {
+    font-family: '${font_id}';
+    font-style: normal;
+    font-weight: ${weight};
+    font-display: swap;
+    src: url(https:/font.emtech.cc/file/_generated/${state.static_font_version}-${font_id}-${weight}/${paddedPack}.woff2) format('woff2');
+    unicode-range: ${p.unicodeRanges};
+  }\n`;
+	});
+
+	writeCssFile(font_id, weight, cssBlocks, state);
+	return cssBlocks;
 }
+
 export { make_css_code_point };
