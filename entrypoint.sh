dabbb6855a9bfa8b1c5de344a7a05e4742c03a26
EachChen <iach526526@gmail.com>
2026-01-11 00:18:48 +0800

    fix: remove unnecessary directory change in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,6 @@ curl -O https://dl.min.io/client/mc/release/linux-amd64/mc
 chmod +x mc && mv mc /usr/local/bin/ 
 echo "Log: MinIO Client installed!"
 mc --version && mc alias set emfont $MINIO_ENDPOINT $MINIO_USERNAME $MINIO_PASSWORD
-cd app/
 # test file download
 mc cp emfont/zeabur/css/975HazyGo/200.css ./
 exec "$@"
